cffedf632b7d92fa3a623e391fb7593447a7f7b6
DeanEarl <deanfailed@gmail.com>
2018-04-22 18:31:16 +0800

    added two more heading to the item table

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -3,8 +3,6 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">Edit Item #{{ $item->id }}</div>
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,11 +1,98 @@
 @extends('layouts.app')
+@section('styles')
 
+    <style>
+        @import "https://fonts.googleapis.com/css?family=Montserrat:300,400,700";
+        .rwd-table {
+            margin: 1em 0;
+            min-width: 300px;
+        }
+        .rwd-table tr {
+            border-top: 1px solid #ddd;
+            border-bottom: 1px solid #ddd;
+        }
+        .rwd-table th {
+            display: none;
+        }
+        .rwd-table td {
+            display: block;
+        }
+        .rwd-table td:first-child {
+            padding-top: .5em;
+        }
+        .rwd-table td:last-child {
+            padding-bottom: .5em;
+        }
+        .rwd-table td:before {
+            content: attr(data-th) ": ";
+            font-weight: bold;
+            width: 6.5em;
+            display: inline-block;
+        }
+        @media (min-width: 480px) {
+            .rwd-table td:before {
+                display: none;
+            }
+        }
+        .rwd-table th, .rwd-table td {
+            text-align: left;
+        }
+        @media (min-width: 480px) {
+            .rwd-table th, .rwd-table td {
+                display: table-cell;
+                padding: .25em .5em;
+            }
+            .rwd-table th:first-child, .rwd-table td:first-child {
+                padding-left: 0;
+            }
+            .rwd-table th:last-child, .rwd-table td:last-child {
+                padding-right: 0;
+            }
+        }
+
+        h1 {
+            font-weight: normal;
+            letter-spacing: -1px;
+            color: #34495E;
+        }
+
+        .rwd-table {
+            background: #34495E;
+            color: #fff;
+            border-radius: .4em;
+            overflow: hidden;
+        }
+        .rwd-table tr {
+            border-color: #46637f;
+        }
+        .rwd-table th, .rwd-table td {
+            margin: .5em 1em;
+        }
+        @media (min-width: 480px) {
+            .rwd-table th, .rwd-table td {
+                padding: 1em !important;
+            }
+        }
+        .rwd-table th, .rwd-table td:before {
+            color: #dd5;
+        }
+        .red_blink {
+            background-color: indianred;
+            animation: blinker 1s linear infinite;
+        }
+
+        @keyframes blinker {
+            50% {
+                opacity: 0;
+            }
+        }
+
+    </style>
+    @endsection
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
-            <div class="col-md-9">
+            <div class="col-md-12 col-lg-12 col-xl-12">
                 <div class="card">
                     <div class="card-header">Items</div>
                     <div class="card-body">
@@ -13,37 +100,31 @@
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
 
-                        <form method="GET" action="{{ url('/items') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
-                                <span class="input-group-append">
-                                    <button class="btn btn-secondary" type="submit">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </form>
-
                         <br/>
                         <br/>
                         <div class="table-responsive">
-                            <table class="table">
+                            <table class="rwd-table col-md-12">
                                 <thead>
                                     <tr>
-                                        <th>#</th>
                                         <th>Item Name</th>
                                         <th>Unit Of Measure</th>
-                                        <th>Opening</th>
+                                        <th>Current Stock</th>
+                                        <th>Order Period</th>
+                                        <th>Selling Price</th>
+                                        <th>Remarks</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                 @foreach($items as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration or $item->id }}</td>
+                                    @if($item->opening <= $item->safety_stock)
+                                    <tr class="red_blink">
                                         <td>{{ $item->item_name }}</td>
                                         <td>{{ $item->unit_of_measure }}</td>
                                         <td>{{ $item->opening }}</td>
+                                        <td>{{ $item->order_period }}</td>
+                                        <td>{{ $item->sale }}</td>
+                                        <td>{{ $item->remarks }}</td>
                                         <td>
                                             <a href="{{ url('/items/' . $item->id) }}" title="View Item"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                             <a href="{{ url('/items/' . $item->id . '/edit') }}" title="Edit Item"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
@@ -55,6 +136,26 @@
                                             </form>
                                         </td>
                                     </tr>
+                                    @else
+                                        <tr>
+                                            <td>{{ $item->item_name }}</td>
+                                            <td>{{ $item->unit_of_measure }}</td>
+                                            <td>{{ $item->opening }}</td>
+                                            <td>{{ $item->order_period }}</td>
+                                            <td>{{ $item->sale }}</td>
+                                            <td>{{ $item->remarks }}</td>
+                                            <td>
+                                                <a href="{{ url('/items/' . $item->id) }}" title="View Item"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                                <a href="{{ url('/items/' . $item->id . '/edit') }}" title="Edit Item"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                                                <form method="POST" action="{{ url('/items' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Item" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endif
                                 @endforeach
                                 </tbody>
                             </table>
